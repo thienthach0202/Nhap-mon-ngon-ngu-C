9f1564a2c6585b6c63336c990411cf195ea996e5
truongthach <72533521+thienthach0202@users.noreply.github.com>
2020-11-26 08:48:21 +0700

    Add files via upload

---
+++ pheptinh.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main() 
+{
+ int a = 3;
+ int b = 5;
+ printf("Tong: %d + %d = %d",a,b,a+b);
+ printf("\nHieu: %d - %d = %d",a,b,a-b);
+ printf("\nTich: %d * %d = %d",a,b,a*b);
+ printf("\nThuong: %d / %d = %f",a,b,(float)a/b);
+ getchar;
+ return 0;
+}
