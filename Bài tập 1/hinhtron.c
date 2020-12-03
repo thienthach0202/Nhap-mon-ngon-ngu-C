94cefde03750b2824dbc2f71a8505744dd0a4071
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-03 08:42:24 +0700

    Update hinhtron.c

--- Bài tập 1/hinhtron.c
+++ Bài tập 1/hinhtron.c
@@ -13,9 +13,9 @@ int main(void) {
   DT = R * R * PI;
   // Hien thi R, CV, DT theo hang ngang
   // Hien thi R theo hang doc
-  printf("R: %0.1f, R);
+  printf("R: %0.1f\n", R);
   // Hien thi CV theo hang doc
-  printf("CV: %0.1f", CV);
+  printf("CV: %0.1f\n", CV);
   // Hien thi DT theo hang doc       
   printf("DT: %0.1f", DT);
   return 0;
