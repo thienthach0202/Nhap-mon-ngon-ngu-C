878b243da5edd111853977167cd4ee9366fc0f1a
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-03 10:18:27 +0700

    Update tong.c

--- Bài tập 2/tong.c
+++ Bài tập 2/tong.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+int main(void)
+{
+	//Khai bao bien a, b
+	int a, b;
+	//Hien thi NHAP A ra man hinh
+	printf("NHAP A = ");
+	//Xuat NHAP A man hinh 
+	scanf("%d",&a);
+	//Hien thi NHAP B ra man hinh
+	printf("NHAP B = ");
+	//Xuat NHAP B ra man hinh
+	scanf("%d",&b);
+	//Hien thi tong cua a + b
+	printf("a + b = %d", a + b );
 
-int main(void) {
-  int a,b ;
-  printf("Nhap a=");
-  scanf("%d",&a);
-  printf("Nhap b=");
-  scanf("%d",&b);
-  printf (" a + b = %d",a + b);
-  return 0;
+    return 0;
 }
