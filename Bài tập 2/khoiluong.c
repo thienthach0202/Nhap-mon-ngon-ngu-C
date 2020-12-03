643ec7209c09b5a3c693d9df56e57a762cab890a
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-03 11:03:40 +0700

    Add files via upload

---
+++ Bài tập 2/khoiluong.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+  // Khai bao bien m(kg), g kieu int
+	int m, g;
+	//Hien thi nhap khoi luong m ra man hinh
+	printf("Nhap khoi luong m : ");
+	//Xuat nhap khoi luong m man hinh 
+	scanf("%d", &m);
+  printf("kg = %dg\n", m * 1000);
+	//Hien thi nhap khoi luong g ra man hinh
+	printf("Nhap khoi luong g : ");
+	//Xuat nhap khoi luong g ra man hinh
+	scanf("%d", &g);
+  printf("g = %dkg\n", g / 1000);
+	//Hien thi tieu de Kg và gia tri
+	printf("tieu de Kg\t    tieu de G\n");
+  printf(" %d", m);
+  printf("           =  %d", g);
+    return 0;
+}
