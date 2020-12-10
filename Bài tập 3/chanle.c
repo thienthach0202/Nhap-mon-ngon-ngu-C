3012d3079da819af2e1084e988f9b58f36675a26
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-10 09:57:24 +0700

    Update chanle.c

--- Bài tập 3/chanle.c
+++ Bài tập 3/chanle.c
@@ -9,12 +9,12 @@ int main() {
   if(n % 2 == 0){
       printf("%d la so chan\n",n);
   //hien thi n la so chan ra man hinh    
-  } else if(n == 0) {
-      printf("n = 0");
-  // hien thi n la 0 ra man hinh    
-  }else if(n % 2 != 0) {
+  } else if(n % 2 != 0) {
       printf("%d la so le\n",n);
   // hien thi n la so le ra man hinh    
+  }else if("n == 0") {
+      printf(n = 0);
+  // hien thi n bang 0 ra man hinh    
   }    
   // hien thi noi dung
   getchar();
