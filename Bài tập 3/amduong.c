cc4a281b19a85aec5066d686c45bb4c805ddfe83
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-10 08:33:40 +0700

    Add files via upload

---
+++ Bài tập 3/amduong.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int a;
+  // hien thi a ra man hinh 
+  printf("nhap a = ");
+  scanf("%d", &a);
+  // kiem tra a la duong hay am
+  if(a >= 0){
+  printf("%d la so khong am ",a);
+  } else {
+  printf("%d la so am ",a);  
+  }
+  // hien thi noi dung
+  printf("%d", a);
+
+  getchar();
+  return 0;
+}
