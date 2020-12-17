6c467a6cbb8de5bc39cb7cca44b337d00b132091
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-17 08:44:34 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -4,18 +4,18 @@ int main() {
   // khai bao bien dongia, soluong, thanhtien
   int dongia, soluong, thanhtien;
   // xuat nhap dongia ra man hinh
-  printf("nhap dongia duoc mua la:");
+  printf("nhap dongia:");
   scanf("%d", &dongia);
   // xuat nhap soluong ra man hinh
   printf("nhap soluong duoc mua la:");
   scanf("%d", &soluong);
   thanhtien = dongia * soluong; 
-  if (soluong >= 5){
-  printf("xuat thanhtien = %d\n", thanhtien *75/100);
-  // Nếu số lượng mua từ 5 trở lên thì sẽ giảm giá 25%
-}else{
-  printf("xuat thanhtien = %d\n", thanhtien);
-  // Ngược lại số lượng mua bé hơn 5 thì sẽ không giảm giá
-}
+  if (soluong >= 5 && dongia >= 500){
+  printf("xuat thanhtien là %d", thanhtien * 50/100);
+  // nếu soluong mua từ 5 trở lên và dongia lớn hơn 500 thì sẽ giảm giá 50%
+  }else{
+  printf("xuat thanhtien là %d", thanhtien);
+  // Ngược lại soluong nhỏ hơn 5 thì sẽ không giảm giá
+  }
   return 0;
 }
