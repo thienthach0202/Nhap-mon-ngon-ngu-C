9477b624e6d0c2ffaddd70e6831eb763abdc1e3c
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-19 20:41:03 +0700

    Update thang.c

--- Bài tập 3/thang.c
+++ Bài tập 3/thang.c
@@ -1,18 +1,38 @@
+
 #include <stdio.h>
 
-int main(void) {
-  char thang;
-    printf("nhap thang:");
-    scanf("%c", &thang);
-    switch(thang){
-      case 1:
-        printf("31 ngay");
-      case 2:
-        printf("28 ngay");
-      case 4:
-        printf("30 ngay");  
-    default:
-      printf("nhap thang");
-    }    
-  return 0;
+int main(void):{ 
+    //Khai bao bien 
+    char pt;
+    int thang;
+    //hien thi ra man hinh
+         printf("Nhap thang:\n");
+         //Nhap so lieu tu ban phim
+         scanf("%d", &thang);
+         switch(thang)
+	{
+	    case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+	 //Hien thi ra man hinh 
+         printf("   Co 31 ngay");break;
+	    case 2:	
+	 //Hien thi ra man hinh 
+	 printf("  Co 28 ngay");break;
+	    case 4:
+            case 6:
+            case 9:
+            case 11:
+         //Hien thi ra man hinh
+	 printf(" Co 30 ngay");break;
+	 default:
+         //Hien thi ra man hinh khi nhap sai
+	 printf("Vui long nhap thang");
+	 }
+  	 getchar();
+    return 0;
 }
