0a86371d328071e0d3027e668e14170578fbbdf6
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-24 10:56:22 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-// khai bao bien a  
-int tong;
-// tong = 1 + 2 + 3 + ... + 10
-tong = 0;    
+// khai bao bien tong, n, dem 
+int tong, n, dem;
+// tong = 1 + 2 + 3 + ... + n
+tong = 0;
+// hien thi nhap so n ra man hinh
+printf("nhap so n:");
+scanf("%d", &n);    
 // vong lap for
-int dem;
-for(dem = 1; dem <= 10; dem = dem + 1){
+for(dem = 1; dem <= n; dem = dem + 1){
   tong += dem;
 }
-// hien thi tongla ra man hinh
+// hien thi tongla ra man hinh 
 printf("tongla :%d", tong);
   return 0;
 }
