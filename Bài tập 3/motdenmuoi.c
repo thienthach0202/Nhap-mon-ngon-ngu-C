3c60d7f39e26c7051fb40e3b0ad80acbbdec1a2e
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-24 09:29:24 +0700

    Update motdenmuoi.c

--- Bài tập 3/motdenmuoi.c
+++ Bài tập 3/motdenmuoi.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
+// khai bao bien a  
 int a;
 
-  for (a = 1; a < 11; ++a)
-  {
+  for (a = 1; a <= 10; a = a + 1)
+  
   // Hien thi 1 den 10 theo khoang trang ra man hinh  
     printf("%d ", a);
-  }
-  int count = 1;
-  while (count <= 10)
-  { 
+  
+  for (a = 1; a <= 10; a = a + 1)
+   
   // Hien thi 1 den 10 ra man hinh 
-    printf("%d\n", count);
-    count++;  
-  }
+    printf("%d\n\n", a);  
   return 0;
 }
