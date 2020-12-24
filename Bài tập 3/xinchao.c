3083f5b9d818797351a725c363cfe17c52bc0b06
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-24 08:33:32 +0700

    Update xinchao.c

--- Bài tập 3/xinchao.c
+++ Bài tập 3/xinchao.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
+  // khai tao bien 
   int count = 1;
   while (count <= 10)
   { 
+  // Hien thi xin chao ra man hinh 
     printf("Xin chao %d\n", count);
     count++;  
   }
+  // Hien thi vong lap thanh cong ra man hinh 
   printf("Vong lap thanh cong");
 }
