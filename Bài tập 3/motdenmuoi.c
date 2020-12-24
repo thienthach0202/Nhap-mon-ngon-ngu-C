a56ba2834eb694abaf71731c09440ac42b70ce82
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2020-12-24 08:49:34 +0700

    Create motdenmuoi.c

---
+++ Bài tập 3/motdenmuoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+int a;
+
+  for (a = 1; a < 11; ++a)
+  {
+  // Hien thi 1 den 10 theo khoang trang ra man hinh  
+    printf("%d ", a);
+  }
+  int count = 1;
+  while (count <= 10)
+  { 
+  // Hien thi 1 den 10 ra man hinh 
+    printf("%d\n", count);
+    count++;  
+  }
+  return 0;
+}
