e46adca5fe2d93c82f67ba0dae53d813ebf71240
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-03 15:24:39 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -5,16 +5,24 @@ int main(){
     int n, i, tong;
     tong = 0;
     // Hien thi Nhap n ra man hinh
-    printf("Nhap n: ");
-    scanf("%d",&n);
-    // Vong lap 
-    for(;n > 0;){
+    printf("Nhap n > 0: ");
+    scanf("%d", &n);
+      // neu n be hon 0 thi chuong trinh khong thuc hien 
+      if(n < 0){
+      // Hien thi FAIL!!! Yeu cau nhap n > 0 ra man hinh
+      printf("FAIL!!! Yeu cau nhap n > 0\n");
+      }
+      // Vong lap 
+      for( ;n > 0; ){
+        // tinh i 
         i = n % 10;
+        // tinh tong 
         tong += i;
-        n /= 10;
-    }    
-    // Hien thi ket qua ra man hinh
-    printf("%d",tong);
-
+        // tinh n
+        n = n / 10;
+        
+    // Hien thi noi dung
+    printf("Tong cua cac chu so nguyen = %d ",tong);
+    }
     return 0;
 }
