dd5ae3b12c4c56452533aa9404cada27a8944000
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-03 18:13:33 +0700

    Update kt_02.c

--- Bài tập 3/kt_02.c
+++ Bài tập 3/kt_02.c
@@ -2,35 +2,57 @@
  
 int main()
 {
-    // Khai bao bien a, b, c, d 
-    int a, b, c, d;
-    // /hien thi Nhap vao 3 so a, b, c ra man hinh
-    printf("Nhap vao 3 so a, b, c: \n");
-    scanf("%d%d%d",&a,&b,&c);
+    // Khai bao bien a, b, c, delta, x, x1, x2
+    int a, b, c, delta, x, x1, x2;
+    printf("Giai phuong trinh bac 2: ax2 + bx + c = 0\n");
+    // /hien thi Nhap a, b, c ra man hinh
+    printf("Nhap a : ");
+    scanf("%d",&a);
+    printf("Nhap b : ");
+    scanf("%d",&b);
+    printf("Nhap c : ");
+    scanf("%d",&c);
+    // neu a = 0 thi ptrinh co dang bx+c=0 -> chinh la giai PT bac 1
     if(a == 0){
-    // Hien thi Phuong trinh co vo so nghiem ra man hinh
-    printf("Phuong trinh co vo so nghiem!");
-    }
-    else if(b == 0){
-    // Hien thi Phuong trinh vo nghiem ra man hinh
-    printf("Phuong trinh vo nghiem!");
-    }
-    else if(c == 0){
-    // Hien thi Phuong trinh co nghiem duy nhat la ra man hinh
-    printf("Phuong trinh co nghiem duy nhat la: %d",-c/b);
-    }
-    d=b*b-4*a*c;
-    if (d < 0){
-    // Hien thi Phuong trinh vo nghiem ra man hinh
-    printf("Phuong trinh vo nghiem!!!");
-    }
-    else if (d == 0){
-    // Hien thi Phuong trinh co nghiem kep la ra man hinh
-    printf("Phuong trinh co nghiem kep la: %d",-b/(2*a));
-    }
-    else{
-    // Hien thi Phuong trinh co 2 nghiem phan biet la ra man hinh
-    printf("Phuong trinh co 2 nghiem phan biet la: %d,%d",(-b+(d))/(2*a),(-b-(d))/(2*a));    
-    }    
+        // neu b bang 0 thi phuong trinh vo nghiem 
+        if(b == 0){
+            // neu c bang 0 thi phuong trinh co vo nghiem 
+            if(c == 0)
+        // hien thi Phuong trinh co vo so nghiem ra man hinh
+        printf("Phuong trinh co vo so nghiem!");
+            else
+        // Hien thi Phuong trinh vo nghiem ra man hinh
+        printf("Phuong trinh vo nghiem!");
+      }else{
+        // Hien thi Phuong trinh co nghiem x ra man hinh
+        printf("Phuong trinh co nghiem la %d", -c / b);
+      }
+   }else{
+        // Khi a khac 0 , thuc hien tinh delta theo 3 truong hop 
+        // Cong thuc tinh delta khi delta be hon 0
+        delta = b*b - 4*a*c;
+        // Cong thuc tinh delta khi delta bang 0
+        x = -b/(2*a);
+    // TH1: neu delta be hon 0 thi phuong trinh vo nghiem
+    if(delta < 0)
+        // hien thi Phuong trinh vo nghiem ra man hinh
+        printf("Phuong trinh vo nghiem!");
+    // TH2: neu delta bang 0 thi phuong trinh co 1 nghiem kep 
+    else if(delta == 0)
+        // Hien thi Phuong trinh co nghiem kep la ra man hinh
+        printf("Phuong trinh co 1 nghiem kep la: %d", x);
+    // TH3: neu delta lon hon 0 thi phuong trinh co 2 nghiem phan biet
+    // delta > 0
+    else {
+        // Cong thuc tinh delta khi delta lon hon 0 
+        x1 = (-b+(delta))/(2*a);
+        x2 = (-b-(delta))/(2*a);
+        // Hien thi Phuong trinh co 2 nghiem phan biet la ra man hinh
+        printf("Phuong trinh co 2 nghiem phan biet x1 = %d, x2 = %d",x1, x2); 
+    }     
+   } 
+      // Hien thi noi dung
+      printf("\n~~M I S S I O N    C O M P L E T E~~");
+
   return 0;
 }
