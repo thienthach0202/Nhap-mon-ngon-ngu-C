7432e50743f8eaf33f2523795ac9b863b3a52124
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-07 09:50:09 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -7,14 +7,14 @@ int main(void) {
   int a, b;
   // Vong lap 
   for(a = 2; a <= 10; a++){
-  // Hien thi X ra man hinh
-  printf("\n--------X%d--------\n", a);
+  // Hien thi bang nhan ra man hinh
+  printf("\nbang nhan %d\n", a);
   // Vong lap
   for(b = 1; b < 10; b++){
   // Hien thi ket qua bang cuu chuong ra man hinh
   printf("\t%d x %d = %d\n", a, b, a*b);
   }
   }
-  printf("\n\n E N D       G A M E ");
+  
   return 0;
 }
