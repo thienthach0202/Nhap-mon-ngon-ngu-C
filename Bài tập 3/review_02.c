a8bba321759f8b4aaefc32a88419bdc7c4ec936e
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-07 09:31:05 +0700

    Update review_02.c

--- Bài tập 3/review_02.c
+++ Bài tập 3/review_02.c
@@ -1,89 +1,59 @@
 #include <stdio.h>
 
-
 int main() 
 {
-	//Khai bao bien a, b, i
     int a, b, i;
-    //Hien thi ket qua ra man hinh
-    printf("Nhap so\n");
     scanf("%d\n%d", &a, &b);
-    
-  
-	//Vong lap for
-for(i = a; i <= b; i++)
-
-	//Dieu kien 1 be hon hoac bang i va i be hon hoac bang 9
-if(1 <= i && i <= 9)
+for(i=a;i<=b;i++)
 {
-	//i be hon hoac bang 1
-if(i <= 1)
+  if(1<=i && i<=9)
 {
-	//Hien thi ket qua ra man hinh la one
-    printf("One\n");
-}	//i be hon hoac bang 2
-else if(i <= 2)
+  if(i<=1)
 {
-	//Hien thi ket qua ra man hinh la Two
-    printf("Two\n");
+    printf("one\n");
 }
-	//i be hon hoac bang 3 
-else if(i <= 3)
+  else if(i<=2)
 {
-		//Hien thi ket qua ra man hinh la Three
-    printf("Three\n");
+    printf("two\n");
 }
-	//i be hon hoac bang 4 
-else if(i <= 4)
+  else if(i<=3)
 {
-		//Hien thi ket qua ra man hinh la Four
-    printf("Four\n");
+    printf("three\n");
 }
-	//i be hon hoac bang 5 
-else if(i <= 5)
-{	
-	//Hien thi ket qua ra man hinh la Five
-    printf("Five\n");
-}
-	//i be hon hoac bang 6 
-else if(i <= 6)
+  else if(i<=4)
 {
-		//Hien thi ket qua ra man hinh la Six
-    printf("Six\n");
+    printf("four\n");
 }
-	//i be hon hoac bang 7
-else if(i <= 7)
+  else if(i<=5)
 {
-		//Hien thi ket qua ra man hinh la Seven
-    printf("Seven\n");
+    printf("five\n");
 }
-	//i be hon hoac bang8
-else if(i <= 8)
+  else if(i<=6)
 {
-		//Hien thi ket qua ra man hinh la Eight
-	printf("Eight\n");
+    printf("six\n");
 }
-	//i be hon hoac bang 9
-else{
-		//Hien thi ket qua ra man hinh la Nine
-    printf("Nine\n");
+  else if(i<=7)
+{
+    printf("seven\n");
+}
+  else if(i<=8)
+{
+    printf("eight\n");
+}
+  else{
+    printf("nine\n");
 }
 }
-else
+  else{
+ if(i%2==0)
 {
-// i chia het cho 2
-if(i%2==0){
-    // Hien thi even ra man hinh
     printf("even\n");
 }
-// nguoc lai 
-else
+  else
 {
-    // Hien thi odd ra man hinh
     printf("odd\n");
 }
 }
-
-
+}
 return 0;
 }
