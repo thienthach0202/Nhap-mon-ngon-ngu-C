0d90551043e0a7fb9d2890741e68c8cac80988b8
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-07 09:31:58 +0700

    Update review_01.c

--- Bài tập 3/review_01.c
+++ Bài tập 3/review_01.c
@@ -1,63 +1,30 @@
 #include <stdio.h>
+
+
 int main()
-
 {
-	//Khai bao bien n
-int n;
-	//Hien thi Nhap n ra ban hinh
-	printf("Nhap n : \n");
-	//Nhap so tu ban phim
-  scanf("%d", &n);
-
-if (n <= 1 )
-{
-	//Hien thi One ra man hinh
-	printf("One\n");
-}	
-else if (n <= 2)
-{	
-  //Hien thi Two ra man hinh
-	printf("Two\n");
-}
-else if (n <= 3)
-{
-  //Hien thi Three ra man hinh
-	printf("Three\n");
-}
-else if (n <= 4)
-{
-  //Hien thi Four ra man hinh
-	printf("Four\n");
-}
-else if (n <= 5)
-{
-  //Hien thi Five ra man hinh
-	printf("Five\n");
-}
-else if (n <= 6)
-{
-  //Hien thi Six ra man hinh
-	printf("Six\n");
-}
-else if (n <= 7)
-{
-  //Hien thi Seven ra man hinh
-	printf("Seven\n");
-}
-else if (n <= 8)
-{
-  //Hien thi Eight ra man hinh
-	printf("Eight\n");
-}
-else if (n <= 9)
-{
-  //Hien thi Nine ra man hinh
-	printf("Nine\n");
-}
-else  
-{
-  //Hien thi Greater than 9 ra man hinh
-	printf("Greater than 9\n");
-}
-return 0;
+    int n;
+    scanf("%d", &n);
+    if (n<=1){
+        printf("one\n");
+    }else if(n<=2){
+        printf("two\n");
+    }else if(n<=3){
+        printf("three\n");
+    }else if(n<=4){
+        printf("four\n");
+    }else if(n<=5){
+        printf("five\n");
+    }else if(n<=6){
+        printf("six\n");
+    }else if(n<=7){
+        printf("seven\n");
+    }else if(n<=8){
+        printf("eight\n");
+    }else if(n<=9){
+        printf("nine\n");
+    }else{
+        printf("Greater than 9");
+    }
+    return 0;
 }
