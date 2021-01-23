dbeef13c2ec5c743b022d6f9ebffc3b3f25a0065
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-23 14:02:03 +0700

    Create ham_01.c

---
+++ Bài tập 5/ham_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//khai bao ham sum
+int sum(int a, int b, int c);
+
+int main(void) {
+  int x,y,z;
+  printf("nhap x: ");scanf("%d", &x);
+  printf("nhap y: ");scanf("%d", &y);
+  printf("nhap z: ");scanf("%d", &z);
+  printf("x + y + z = %d", sum(x,y,z));//khoi ham sum 
+  return 0;
+}
+int sum(int a, int b, int c){//dinh nghia ham sum
+  return a + b + c;
+}
