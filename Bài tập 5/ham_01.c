1dfe8090077ac2443d970de3a02d39465c3cb063
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-23 15:21:43 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -16,20 +16,17 @@ int sum(int a, int b, int c){//dinh nghia ham sum
 
 
 #include <stdio.h>
-//khai bao ham sum
+
 void sum();
 int main(void) {
   sum();
   return 0;
 }
 void sum(){
-  int x;
-  int tong = 0;
-  for(int i=1;i<=3;i++){
-    tong = tong + i;
-  printf("nhap so nguyen thu %d: ",i);
-  scanf("%d",&x);
-  } 
-  printf("\n");
-  printf("x + y + z = %d",tong);//khoi ham sum
+  int x,y,z;
+  printf("nhap x: ");scanf("%d", &x);
+  printf("nhap y: ");scanf("%d", &y);
+  printf("nhap z: ");scanf("%d", &z);
+  printf("x + y + z = %d",x + y + z);
+
 }
