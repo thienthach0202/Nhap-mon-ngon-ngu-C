8df0e5f43d8745159c6cd067eb6a4f941266a8bc
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-28 15:19:48 +0700

    Update TH_02.c

--- Bài tập 5/TH_02.c
+++ Bài tập 5/TH_02.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(void) {
+// khai bao bien s, n
+int s, n;
+// s = 1 + 2 + 3 + ... + n
+s = 0;
+// hien thi nhap so n ra man hinh
+printf("nhap so n:");
+scanf("%d", &n);    
+// vong lap for
+for(int i = 1;i <= n;i++){
+  //tinh s
+  s = s + i;
+}
+// hien thi noi dung 
+printf("tong: %d", s);
+  return 0;
+}
