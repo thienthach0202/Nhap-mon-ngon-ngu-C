90dc6434a2edb42c63a7d17c4f76f72508b21f4b
Trương Thạch <72533521+thienthach0202@users.noreply.github.com>
2021-01-28 14:18:03 +0700

    Update ham_03.c

--- Bài tập 5/ham_03.c
+++ Bài tập 5/ham_03.c
@@ -1,13 +1,15 @@
 //Khoi khai bao
 #include<stdio.h>
-void Nhap_pt_mang();
-void Hien_thi_pt_mang();
-void Hien_thi_tong_chan();
-void Dem_chan_le_0();
+void Nhap_pt_mang(int a[]);
+int n;
+void Hien_thi_pt_mang(int a[]);
+int n;
+void Hien_thi_tong_chan(int a[]);
+void Dem_chan_le_0(int a[]);
 
 //khoi ham main
 int main(){
-int n;
+//int n;
 printf("Nhap n = ");
 scanf("%d", &n);
 int a[n];
@@ -17,19 +19,20 @@ Hien_thi_tong_chan(a);
 Dem_chan_le_0(a);
 return 0;
 }
-void Nhap_pt_mang(int n, int i){
-  int a[n];
-    for(i=0;i<n;i++){
+//khoi dinh nghia ham 
+void Nhap_pt_mang(int a[]){
+    for(int i=0;i<n;i++){
       printf("Nhap_pt_mang(a): ");
       scanf("%d", &a[i]);
 }
-void Hien_thi_pt_mang(){
+void Hien_thi_pt_mang(int a[]){
   printf("Hien_thi_pt_mang(a): ");
   for(i=0;i<n;i++){
     printf("%d ", a[i]);
-    }
+  }
+    
 }
-void Hien_thi_tong_chan(){
+void Hien_thi_tong_chan(int a[]){
   int tongchan = 0;
     for(int j=0;j<=n;j++){
       if(a[j]%2==0)
@@ -38,8 +41,16 @@ void Hien_thi_tong_chan(){
     printf("\ntongchan: %d",tongchan);
 
 }
-void Dem_chan_le_0(){
-  
+void Dem_chan_le_0(int a[]){
+  int demchan = 0, demle = 0, dem0 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            demchan++;
+        if(a[i] % 2 == 1)
+            demle++;
+        if(a[i] == 0)
+            dem0++;
+    }
+    printf("\nCo %d so chan, %d so le va %d so 0", demchan, demle, dem0);
 }
-
-// khoi dinh nghia ham
